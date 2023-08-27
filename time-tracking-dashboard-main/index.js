73f701cd39fab24e11d57f7b6cb75f142fb88fdf
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-27 15:20:54 -0300

    teste documentação

--- time-tracking-dashboard-main/index.js
+++
@@ -1,9 +0,0 @@
-let activityInformations = document.querySelector(".activity-informations")
-
-fetch("data.json").then((response) => {
-    response.json().then((data) => {
-        data.activity.map((activity) =>{
-            activityInformations.innerHTML += `<div> ${activity.title} </div>`
-        })
-    })
-})
