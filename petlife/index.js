5ce07e944e37e5b8aa87f24416c9b6e9241f0b9a
Mich <125368412+michellycruz@users.noreply.github.com>
2023-11-19 11:42:10 -0300

    navbar responsivo

--- petlife/index.js
+++ petlife/index.js
@@ -1,12 +1,44 @@
-class MobileNavbar{
-    constructor(mobileMenu, nav){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.nav = document.querySelector(nav);
-        this.navLinks = document.querySelector(navLinks);
-        this.activeClass = "active";
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
     }
-
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click", this.handleClick);
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
     }
-}
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list a",
+  );
+  mobileNavbar.init();
