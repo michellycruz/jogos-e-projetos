855d2e8d6c3076128581896caf39b7c740ca4ed9
Mich <michxcrz@gmail.com>
2023-12-19 17:12:01 -0300

    ?

--- jogo_da_memoria/js/game.js
+++ jogo_da_memoria/js/game.js
@@ -22,15 +22,40 @@ const createElement = (tag, className) =>{
 let firstCard = '';
 let secondCard = '';
 
+const checkEndGame = () => {
+    const disabledCards = document.querySelector('.disabled-card');
+
+    if(disabledCards.length === 20){
+        alert('Parabéns, você conseguiu!');
+    }
+}
+
 const checkCards = () => {
     const firstCharacter = firstCard.getAttribute('data-character');
     const secondCharacter = secondCard.getAttribute('data-character');
 
     if(firstCharacter === secondCharacter){
 
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
+
+        firstCard = '';
+        secondCard = '';
+
+        checkEndGame();
+
     } else {
-        firstCard.classList.remove('reveal-card');
-        secondCard.classList.remove('reveal-card');
+
+        setTimeout(()=> {
+            
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+
+        }, 500);
+
     }
 }
 
