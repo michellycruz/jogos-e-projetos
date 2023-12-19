8d386deba2f3185020b6398cecfd737564990377
Mich <michxcrz@gmail.com>
2023-12-19 10:30:19 -0300

    continuação jogo da memoria

---
+++ jogo_da_memoria/js/game.js
@@ -0,0 +1,10 @@
+const createCard = () => {
+
+    const card = document.createElement('div');
+    const front = document.createElement('div');
+    const back = document.createElement('div');
+
+    card.className = 'card';
+    front.className = 'front';
+    back.className = 'back';
+}
--- jogo_da_memoria/js/login.js
+++ jogo_da_memoria/js/login.js
@@ -1,13 +1,22 @@
 const input = document.querySelector('.loginInput');
 const button = document.querySelector('.loginBtn');
+const form = document.querySelector('.loginForm');
 
 
 const validateInput = ({ target }) => {
     if(target.value.length > 2){
         button.removeAttribute('disabled');
-    } else{
-        button.setAttribute('disabled', '');
+        return;
     }
+    button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('player', input.value);
+
+    window.location = './pages/game.html';
 }
 
 input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
